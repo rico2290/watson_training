813765fe7908c3723a99243bd5cf3ceaaf83d378
rico2290 <rjl220990@gmail.com>
2019-03-14 16:30:57 -0300

    Entidade, Dicionário e Body_request de Watson Machine Learning Understanding

--- NLUApi.py
+++ NLUApi.py
@@ -1,18 +1,46 @@
 import json
-from watson_developer_cloud import NaturalLanguageUnderstandingV1 as NLU
-from watson_developer_cloud.natural_language_understanding_v1 import Features, RelationsOptions #CategoriesOptions, ConceptsOptions,EntitiesOptions, KeywordsOptions, MetadataOptions, , RelationEntity
+import PyPDF2
+from pprint import pprint
+from watson_developer_cloud import NaturalLanguageUnderstandingV1 as NLU 
+from watson_developer_cloud.natural_language_understanding_v1  \
+    import Features, RelationsOptions,EntitiesOptions,MetadataOptions,RelationEntity, SentimentOptions
 
+data = []
+with open('credentials.json', 'r') as file:
+    data = json.load(file)
+#print(json.dumps(data, indent=2))
 
+url = data['credentials']['url']
+version = data['credentials']['version']
+apikey = data['credentials']['apikey']
+model_id = data['credentials']['model_id']
 
-url = 'https://gateway.watsonplatform.net/natural-language-understanding/api'
-version = '2017-11-03'
-apikey = 'P4pI0dZSd40kc9HKMsXhjAtDw80_cMy9_DNiAL2FPxIy'
+pdf_file = 'Decisao_Judicial_Med.pdf'
+ler_pdf = PyPDF2.PdfFileReader(pdf_file)
+for x in range(ler_pdf.getNumPages()):
+    pagina = ler_pdf.getPage(x)
+    #print("Página Numero: {}".format(str(1+ler_pdf.getPageNumber(pagina))))
+    conteudo = pagina.extractText()
+    #print((conteudo))
+  
+#decisao = 'C:\Users\102869\Downloads\STJ_EDC.pdf',
 
 nlu = NLU(version=version,iam_apikey=apikey,url=url)
+url_jusBrasil = 'https://www.jusbrasil.com.br/jurisprudencia/busca?q=fornecimento+de+medicamentos&idtopico=T10000001&idtopico=T10000002&idtopico=T10000007'
 
-texto = 'Minha namorada acaba de ser contratada pela empresa Cloud Computing'
 res = nlu.analyze(
-    text=texto,
-    features=Features(relations=RelationsOptions())).get_result()
+    url=url_jusBrasil,
+    features=Features(relations=RelationsOptions(model=model_id),
+    sentiment=SentimentOptions(targets=['negar provimento']),
+    entities=EntitiesOptions(model=model_id))).get_result()
+    
+
 
 print(json.dumps(res, indent=2))
+
+
+with open('body_request.json','w',encoding='utf-8') as file:
+    ler = json.dumps(res,ensure_ascii=False,indent=1)
+    #ler = json_encode(ler, JSON_UNESCAPED_UNIOCODE)
+    file.write(ler)
+
--- wvr.py
+++ wvr.py
@@ -1,9 +1,12 @@
 from watson_developer_cloud import VisualRecognitionV3 as vr 
 import json
+from flask import jsonify
 
-#Reconhecimento de Imagem iam_apikey = apikey
-wvr = vr( version="2018-01-29",iam_apikey="F_-o_C5ie1HKGU750akv3SsbDwy5dI2yigLVl0TSZVOi",)
-img = wvr.classify(url="https://s2.glbimg.com/3auOxS3cG2mc_H5jFXDpxC7ol-w=/e.glbimg.com/og/ed/f/original/2016/09/12/dr-alan-turing-2956483.jpg")
-print(img)
 
-print((wvr))
+def url_recognizer():
+    wvr = vr( version="2018-03-19",iam_apikey="F_-o_C5ie1HKGU750akv3SsbDwy5dI2yigLVl0TSZVOi",)
+    img = wvr.classify(url="https://s2.glbimg.com/3auOxS3cG2mc_H5jFXDpxC7ol-w=/e.glbimg.com/og/ed/f/original/2016/09/12/dr-alan-turing-2956483.jpg")
+    return print(img)
+    
+
+url_recognizer()
